5b1c729fb395abd74f172cbdcb9744c4a01cc8ad
victor-oli1999 <victoroliveira2016@icloud.com>
2022-10-12 11:18:02 -0300

    Montei a página estática

--- pagina-estatica/src/index.js
+++ pagina-estatica/src/index.js
@@ -1,17 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react"
+import ReactDOM from "react-dom"
+import "./styles.css"
+import ReactLogo from "./images/react-logo.png"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function Nav() {
+    return (
+    <div className="navbar">
+        <div className="react-logo">
+            <img src={ReactLogo} alt="React logo" width="40px" />
+            <h2>ReactFacts</h2>
+        </div>
+        <h3>React Course - Project 1</h3>
+    </div>
+    )
+}
+function MainContent() {
+    return (
+    <div className="MainContent">
+        <h1>Fun facts about React</h1>
+        <ul>
+            <li>Was first released in 2013</li>
+            <li>Was originally created by Jordan Walke</li>
+            <li>Has well over 100K stars on GitHub</li>
+            <li>Is maintained by Facebook</li>
+            <li>Powers thousands of enterprise apps, including mobile apps</li>
+        </ul>
+    </div>
+    )
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function App() {
+    return (
+    <div>
+        <Nav />
+        <MainContent />
+    </div>
+    )
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById("root")
+)
